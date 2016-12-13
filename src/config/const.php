e04bffb479ab5aeb948a57172ca0f63dde11d92c
ulrichpech <u.pech@searchmetrics.com>
2016-12-13 10:53:14 +0100

    codeship config

--- src/config/const.php
+++ src/config/const.php
@@ -1,11 +1,11 @@
 <?php
 
 //codeship
-if(!empty(getenv('DB_NAME'))) {
+if(!empty(getenv('MYSQL_USER'))) {
 	define('DB_HOST', 'localhost');
 	define('DB_USER',  getenv('MYSQL_USER'));
 	define('DB_PWD', getenv('MYSQL_PASSWORD'));
-	define('DB_NAME', getenv('DB_NAME'));
+	define('DB_NAME', 'test');
 }
 else{
 	define('DB_HOST', 'localhost');
