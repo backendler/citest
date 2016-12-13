d9299c2ca239381c24b5cee79a1f0b3684954ae5
ulrichpech <u.pech@searchmetrics.com>
2016-12-13 15:56:46 +0100

    piplines

--- src/config/const.php
+++ src/config/const.php
@@ -7,6 +7,12 @@ if(!empty(getenv('MYSQL_USER'))) {
 	define('DB_PWD', getenv('MYSQL_PASSWORD'));
 	define('DB_NAME', 'test');
 }
+elseif(!empty(getenv('CIRCLE_USERNAME'))){
+	define('DB_HOST', 'localhost');
+	define('DB_USER',  getenv('ubuntu'));
+	define('DB_PWD',  '');
+	define('DB_NAME', 'test');
+}
 else{
 	define('DB_HOST', 'localhost');
 	define('DB_USER', 'root');
