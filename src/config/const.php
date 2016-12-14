955ba294fe56e252fc9a746145e63eaca1ae5b02
ulrichpech <u.pech@searchmetrics.com>
2016-12-14 10:24:58 +0100

    db env config

--- src/config/const.php
+++ src/config/const.php
@@ -7,12 +7,28 @@ if(!empty(getenv('MYSQL_USER'))) {
 	define('DB_PWD', getenv('MYSQL_PASSWORD'));
 	define('DB_NAME', 'test');
 }
+//circle
 elseif(!empty(getenv('CIRCLE_USERNAME'))){
 	define('DB_HOST', 'localhost');
 	define('DB_USER', 'ubuntu');
 	define('DB_PWD',  '');
 	define('DB_NAME', 'test');
 }
+//bitbucket piplines
+elseif(!empty(getenv('BITBUCKET_COMMIT'))){
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PWD',  'root');
+	define('DB_NAME', 'test');
+}
+//travis
+elseif(!empty(getenv('TRAVIS'))){
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PWD', '');
+	define('DB_NAME', 'test');
+}
+//local
 else{
 	define('DB_HOST', 'localhost');
 	define('DB_USER', 'root');
