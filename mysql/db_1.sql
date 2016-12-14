938dae1e8d8b7976656a0ad2ca0d953fe44aa084
ulrichpech <u.pech@searchmetrics.com>
2016-12-14 10:44:45 +0100

    shippable

--- mysql/db_1.sql
+++ mysql/db_1.sql
@@ -1,3 +1,3 @@
 CREATE TABLE test.yeah (`id` INT NOT NULL,`text` VARCHAR(255) NULL, PRIMARY KEY (`id`), UNIQUE INDEX `id_UNIQUE` (`id` ASC));
-INSERT INTO test.yeah (`id`, `text`) VALUES ('', 'yeah yeah from db');
+INSERT INTO test.yeah (`id`, `text`) VALUES (0, 'yeah yeah from db');
 
